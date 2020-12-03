9f96bf4d75e773a2cc34e7603a3ab230716048da
Kamii Akbari <ungewang@gmail.com>
2020-12-03 15:23:27 +0100

    Added server config and connected to database.

--- server.js
+++ server.js
@@ -1,4 +1,34 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+import {connectDatabase} from './config/db.js';
+import env from 'dotenv';
 
 
-const express = require('express');
+
+const app = express();
+env.config();
+app.use(express.json());
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(cors({origin:'http://localhost:3000', allowHeaders:['Content-Type','Authorization'], credentials:true,}))
+
+
+
+
+
+
+
+
+
+
+
+
+connectDatabase();
+const PORT = process.env.PORT || 5000;
+const server = app.listen(
+    PORT,
+    console.log(`Server running on port : ${PORT}`)
+);
